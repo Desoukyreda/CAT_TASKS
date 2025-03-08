5a1105cbe577ea1dfdb2d4691d9a1b009203e944
De Sou Ky <151868296+Desoukyreda@users.noreply.github.com>
2025-03-08 23:34:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Finds and prints the largest number in the array using a loop
+        // int []arr = new int[10];
+        // int max = int.MinValue,pos = -1;
+        // System.Console.WriteLine("Enter elements of array : ");
+        // for(int i =0;i<10;i++)
+        // {
+        //     arr[i]=int.Parse(Console.ReadLine());
+        //     if(arr[i]>max)
+        //     {
+        //         max = arr[i];
+        //         pos = i+1;
+        //     }
+        // }
+        // System.Console.WriteLine($"MAX = {max} , position : {pos}");
+        #endregion
+
+        #region vowel chars
+        
+    //    while(true)
+    //    {
+    //        char c ;
+    //        System.Console.Write("Enter the char : ");
+    //        c = (char)Console.Read();
+    //        if(c<'a')System.Console.WriteLine("Constant");
+    //        else System.Console.WriteLine("VOWEL");
+    //    }
+        #endregion
+
+        #region multiplication table
+        // int num ;
+        // System.Console.WriteLine("enter the number : ");
+        // num = int.Parse(Console.ReadLine());
+        // Console.Write("Enter an integer: ");
+        // for(int i  = 1; i<=12;i++)
+        // {
+        //     Console.WriteLine($"{num} x {i} = {num * i}");
+        // }
+     
+        #endregion
+       
+        #region divide by 3 and 4
+
+        // Console.Write("Enter a number: ");
+        // int number = int.Parse(Console.ReadLine());
+
+        
+        // if (number % 3 == 0 && number % 4 == 0)
+        // {
+        //     Console.WriteLine("Yes");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("No");
+        // }
+        // #endregion
+
+        // #region decimal to binary
+        // Console.Write("Enter a decimal number: ");
+        // int decimalNumber = int.Parse(Console.ReadLine());
+
+        // Console.Write("Binary equivalent: ");
+        
+        
+        // while (decimalNumber > 0)
+        // {
+        //     Console.Write(decimalNumber % 2);
+        //     decimalNumber /= 2;
+        // }
+        #endregion
+
+        #region merge two arrays of the same size sorted in ascending order
+    //   int size = 5; 
+
+        
+    //     int[] array1 = new int[size];
+    //     int[] array2 = new int[size];
+
+        
+    //     Console.WriteLine("Enter elements for the first array:");
+    //     for (int i = 0; i < size; i++)
+    //     {
+    //         Console.Write($"Element {i + 1}: ");
+    //         array1[i] = int.Parse(Console.ReadLine());
+    //     }
+
+        
+    //     Console.WriteLine("Enter elements for the second array:");
+    //     for (int i = 0; i < size; i++)
+    //     {
+    //         Console.Write($"Element {i + 1}: ");
+    //         array2[i] = int.Parse(Console.ReadLine());
+    //     }
+
+        
+    //     int[] mergedArray = new int[size * 2];
+    //     for(int i = 0; i<size;i++)
+    //     {
+    //         mergedArray[i] = array1[i];
+    //     }
+    //     for(int i = size; i<size*2;i++)
+    //     {
+    //         mergedArray[i] = array2[i];
+    //     }
+
+        
+    //     Array.Sort(mergedArray);
+
+        
+    //     Console.WriteLine("Merged and sorted array:");
+    //     foreach (int element in mergedArray)
+    //     {
+    //         Console.Write(element +" ");
+    //     }
+        #endregion
+        
+        #region 
+        //  Console.Write("Enter the size of the array: ");
+        // int size = int.Parse(Console.ReadLine());
+
+        
+        // int[] array = new int[size];
+        // Console.WriteLine("Enter the elements of the array:");
+        // for (int i = 0; i < size; i++)
+        // {
+        //     Console.Write($"Element {i + 1}: ");
+        //     array[i] = int.Parse(Console.ReadLine());
+        // }
+
+        
+        // Console.WriteLine("Array in reverse order:");
+        // for (int i = size - 1; i >= 0; i--)
+        // {
+        //     Console.Write(array[i] + " ");
+        // }
+        #endregion
+    }
+}
