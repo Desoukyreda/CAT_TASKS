bba4b782e18f89482121db48ac9cd6ea7969f7c7
De Sou Ky <151868296+Desoukyreda@users.noreply.github.com>
2025-03-23 00:46:08 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,143 +1,74 @@
-﻿namespace ConsoleApp1;
+﻿using System;
 
 class Program
 {
+    static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+ static void Find(Dictionary<string, int> students, string name)
+    {
+        if (students.ContainsKey(name))
+        {
+            Console.WriteLine($"{name}'s grade: {students[name]}");
+        }
+        else
+        {
+            Console.WriteLine($"Student {name} not found.");
+        }
+    }
+
     static void Main(string[] args)
     {
-        #region Finds and prints the largest number in the array using a loop
-        // int []arr = new int[10];
-        // int max = int.MinValue,pos = -1;
-        // System.Console.WriteLine("Enter elements of array : ");
-        // for(int i =0;i<10;i++)
-        // {
-        //     arr[i]=int.Parse(Console.ReadLine());
-        //     if(arr[i]>max)
-        //     {
-        //         max = arr[i];
-        //         pos = i+1;
-        //     }
-        // }
-        // System.Console.WriteLine($"MAX = {max} , position : {pos}");
-        #endregion
+        #region 1
+        // int x = 10, y = 20;
+        // Console.WriteLine($"Before swap: x = {x}, y = {y}");
+        // Swap(ref x, ref y);
+        // Console.WriteLine($"After swap: x = {x}, y = {y}");
 
-        #region vowel chars
-        
-    //    while(true)
-    //    {
-    //        char c ;
-    //        System.Console.Write("Enter the char : ");
-    //        c = (char)Console.Read();
-    //        if(c<'a')System.Console.WriteLine("Constant");
-    //        else System.Console.WriteLine("VOWEL");
-    //    }
-        #endregion
+        // string str1 = "Hello", str2 = "World";
+        // Console.WriteLine($"\nBefore swap: str1 = {str1}, str2 = {str2}");
+        // Swap(ref str1, ref str2);
+        // Console.WriteLine($"After swap: str1 = {str1}, str2 = {str2}");
+        // Console.Read();
+        #endregion 
 
-        #region multiplication table
-        // int num ;
-        // System.Console.WriteLine("enter the number : ");
-        // num = int.Parse(Console.ReadLine());
-        // Console.Write("Enter an integer: ");
-        // for(int i  = 1; i<=12;i++)
+        #region 2
+        //  Dictionary<string, int> Grades = new Dictionary<string, int>
         // {
-        //     Console.WriteLine($"{num} x {i} = {num * i}");
-        // }
-     
+        //     {"AHMED", 90},
+        //     {"ABAS", 85},
+        //     {"WATAN", 78},
+        //     {"SA3DOLA", 92},
+        //     {"SANTI53", 88}
+        // };
+
+        // Console.WriteLine("Enter a student name to find their grade:");
+        // string Name = Console.ReadLine();
+        // Find(Grades, Name);
+        // Console.Read();
         #endregion
+   
+        #region 3
+       Queue<string> customer = new Queue<string>();
+
+        customer.Enqueue("AHMED");
+        customer.Enqueue("SANTI");
+        customer.Enqueue("SA3DOLA");
+        customer.Enqueue("ABSI");
+        customer.Enqueue("YASER");
+
        
-        #region divide by 3 and 4
-
-        // Console.Write("Enter a number: ");
-        // int number = int.Parse(Console.ReadLine());
-
-        
-        // if (number % 3 == 0 && number % 4 == 0)
-        // {
-        //     Console.WriteLine("Yes");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("No");
-        // }
-        // #endregion
-
-        // #region decimal to binary
-        // Console.Write("Enter a decimal number: ");
-        // int decimalNumber = int.Parse(Console.ReadLine());
-
-        // Console.Write("Binary equivalent: ");
-        
-        
-        // while (decimalNumber > 0)
-        // {
-        //     Console.Write(decimalNumber % 2);
-        //     decimalNumber /= 2;
-        // }
-        #endregion
-
-        #region merge two arrays of the same size sorted in ascending order
-    //   int size = 5; 
-
-        
-    //     int[] array1 = new int[size];
-    //     int[] array2 = new int[size];
-
-        
-    //     Console.WriteLine("Enter elements for the first array:");
-    //     for (int i = 0; i < size; i++)
-    //     {
-    //         Console.Write($"Element {i + 1}: ");
-    //         array1[i] = int.Parse(Console.ReadLine());
-    //     }
-
-        
-    //     Console.WriteLine("Enter elements for the second array:");
-    //     for (int i = 0; i < size; i++)
-    //     {
-    //         Console.Write($"Element {i + 1}: ");
-    //         array2[i] = int.Parse(Console.ReadLine());
-    //     }
-
-        
-    //     int[] mergedArray = new int[size * 2];
-    //     for(int i = 0; i<size;i++)
-    //     {
-    //         mergedArray[i] = array1[i];
-    //     }
-    //     for(int i = size; i<size*2;i++)
-    //     {
-    //         mergedArray[i] = array2[i];
-    //     }
-
-        
-    //     Array.Sort(mergedArray);
-
-        
-    //     Console.WriteLine("Merged and sorted array:");
-    //     foreach (int element in mergedArray)
-    //     {
-    //         Console.Write(element +" ");
-    //     }
-        #endregion
-        
-        #region 
-        //  Console.Write("Enter the size of the array: ");
-        // int size = int.Parse(Console.ReadLine());
-
-        
-        // int[] array = new int[size];
-        // Console.WriteLine("Enter the elements of the array:");
-        // for (int i = 0; i < size; i++)
-        // {
-        //     Console.Write($"Element {i + 1}: ");
-        //     array[i] = int.Parse(Console.ReadLine());
-        // }
-
-        
-        // Console.WriteLine("Array in reverse order:");
-        // for (int i = size - 1; i >= 0; i--)
-        // {
-        //     Console.Write(array[i] + " ");
-        // }
+        Console.WriteLine("Processing customer service queue:");
+        while (customer.Count > 0)
+        {
+            string details = customer.Dequeue();
+            Console.WriteLine($"Served: {details}");
+        }
+        Console.Read();
         #endregion
     }
 }
+
